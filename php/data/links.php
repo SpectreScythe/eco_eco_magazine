bdc1c2b3d9e87c336080c1335e4a474cd5fc28f8
Abdurrahman Qureshi <124421417+SpectreScythe@users.noreply.github.com>
2024-03-02 00:19:45 +0530

    Adding articles in progress...

---
+++ php/data/links.php
@@ -0,0 +1,8 @@
+<?php
+
+$article_links = [
+    "./entertainment/deadpool.php",
+    "./entertainment/despicable-me.php",
+    "./entertainment/utopia.php",
+    "./entertainment/breaking-bad.php",
+];
