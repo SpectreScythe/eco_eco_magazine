f79ffe2fb5fcf1201208602bdff6c849c180e3f0
Abdurrahman Qureshi <124421417+SpectreScythe@users.noreply.github.com>
2024-03-03 00:40:50 +0530

    QOL - 03/03/2024

--- pages/add/add_article_page.php
+++ pages/add/add_article_page.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>eco eco | Fashion</title>
+    <title>eco eco | Add Article</title>
     <link rel="stylesheet" href="../../styles/globals.css">
     <link rel="stylesheet" href="../../styles/pages/home.css">
     <link rel="stylesheet" href="../../styles/pages/add-article.css">
